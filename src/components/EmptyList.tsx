443d264b578c0f085b1524ee49b0c68f833fe4a4
Humberto Villalpando <betov18x@gmail.com>
2024-03-25 09:47:07 -0600

    added empty list content

--- src/components/EmptyList.tsx
+++ src/components/EmptyList.tsx
@@ -1,5 +1,26 @@
-export const EmptyList = () => {
-  return (
-    <div>EmptyList</div>
-  )
+import { Link } from "react-router-dom";
+
+interface EmptyListProps {
+  title: string;
+  paragraph: string;
+  linkUrl: string;
+  linkText: string;
 }
+
+export const EmptyList = ({
+  title,
+  paragraph,
+  linkUrl,
+  linkText,
+}: EmptyListProps) => {
+  return (
+    <div className="content-wrapper text-center">
+      <img src="/sorry.jpg" alt="Sorry" style={{ maxWidth: 340 }} />
+      <h1> {title} </h1>
+      <p> {paragraph} </p>
+      <Link to={linkUrl} className="btn-primary">
+        {linkText}
+      </Link>
+    </div>
+  );
+};

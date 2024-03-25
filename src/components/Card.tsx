ce7c90c0daee3e581669535696f430cce1520afd
Humberto Villalpando <betov18x@gmail.com>
2024-03-25 09:46:42 -0600

    added restaurant cards functionallity to like and dont like

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,49 @@
-export const Card = () => {
-  return (
-    <div>Card</div>
-  )
+import "./Card.css";
+import { Restaurant } from "../context/RestaurantsContext";
+import heart from "../assets/heart.svg";
+import cross from "../assets/cross.svg";
+
+interface CardProps extends Restaurant {
+  handleLikeOrDontLike: (id: number, option: boolean) => void;
 }
+
+export const Card = ({
+  id,
+  name,
+  description,
+  url,
+  handleLikeOrDontLike: handleLikeOrDontLike,
+}: CardProps) => {
+  return (
+    <div className="card">
+      <img className="card-img" src={url} alt={name} />
+
+      <div className="card-content">
+        <div className="card-info">
+          <h1>{name}</h1>
+          <p>{description}</p>
+        </div>
+
+        <div className="btn-actions">
+          <button
+            onClick={() => handleLikeOrDontLike(id, false)}
+            className="btn dont-like"
+            title="Don't like this restaurant"
+            aria-label="Don't like this restaurant"
+          >
+            <img src={cross} alt="cross" />
+          </button>
+
+          <button
+            onClick={() => handleLikeOrDontLike(id, true)}
+            className="btn like"
+            title="Like this restaurant"
+            aria-label="Like this restaurant"
+          >
+            <img src={heart} alt="heart" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/components/CardStack.tsx
+++ src/components/CardStack.tsx
@@ -1,5 +1,61 @@
+import { useContext } from "react";
+import { Card } from "./Card";
+import { EmptyList } from "./EmptyList";
+import { Restaurant, RestaurantsContext } from "../context/RestaurantsContext";
+
 export const CardStack = () => {
+  const { restaurants, setRestaurants } = useContext(RestaurantsContext);
+  const availableRestaurants = restaurants.filter(
+    (r) => r.isRestaurantLiked === null
+  );
+
+  const handleLikeOrDontLike = (id: number, isRestaurantLiked: boolean) => {
+    const updatedRestaurants = restaurants.map((r) => {
+      if (r.id === id) {
+        return { ...r, isRestaurantLiked: isRestaurantLiked };
+      }
+      return r;
+    });
+
+    showNextCard(updatedRestaurants, isRestaurantLiked);
+  };
+
+  const showNextCard = (
+    updatedRestaurants: Restaurant[],
+    isRestaurantLiked: boolean
+  ) => {
+    const cardStack = document.querySelector(".content-wrapper");
+    if (!cardStack) return;
+    const topCard = cardStack.querySelector(".card:last-child") as HTMLElement;
+    if (topCard) {
+      topCard.style.transform = isRestaurantLiked
+        ? "translateX(150%)"
+        : "translateX(-150%)";
+      setTimeout(() => {
+        setRestaurants(updatedRestaurants);
+      }, 300);
+    }
+  };
+
+  if (availableRestaurants.length === 0)
+    return (
+      <EmptyList
+        title="There are no more restaurants :("
+        paragraph="But you can still view all the restaurants you've liked!"
+        linkUrl="/your-likes"
+        linkText="View Liked Restaurants"
+      />
+    );
+
   return (
-    <div>CardStack</div>
-  )
-}
+    <div className="content-wrapper">
+      {availableRestaurants.map((restaurant) => (
+        <Card
+          key={restaurant.id}
+          handleLikeOrDontLike={handleLikeOrDontLike}
+          {...restaurant}
+        />
+      ))}
+    </div>
+  );
+};
